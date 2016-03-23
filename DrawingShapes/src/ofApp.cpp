a8afad58eb8795fc196b69d15e5323bd137937f8
Roy Macdonald <macdonald.roy@gmail.com>
2016-03-23 15:12:26 +0800

    update Drawing Shapes

--- DrawingShapes/src/ofApp.cpp
+++ DrawingShapes/src/ofApp.cpp
@@ -1,68 +1,67 @@
 #include "ofApp.h"
 
-//--------------------------------------------------------------
+//--------------------------------------------------
 void ofApp::setup(){
     seed = 100;
 }
-
-//--------------------------------------------------------------
-void ofApp::update(){
-
-}
-
-//--------------------------------------------------------------
+//--------------------------------------------------
+void ofApp::update(){}
+// examples https://github.com/ofcourseio/OF_examples_spring2016
+//--------------------------------------------------
 void ofApp::draw(){
 
     ofDrawRectangle(100,100,100,100);
-    ofSeedRandom(seed);
+    
+    ofSetColor(100);
+    //ofSeedRandom(seed);
+
     ofPoint p1(ofRandomWidth(), ofRandomHeight());
     ofPoint p2(ofRandomWidth(), ofRandomHeight());
     ofPoint p3(ofRandomWidth(), ofRandomHeight());
 
-    ofSetColor(ofColor::pink);
+    ofSetColor(ofColor::yellow);
+    
     ofDrawTriangle(p1, p2, p3);
+    
     ofSetColor(ofColor::sienna);
+    ofSetColor(200,0,0);//red
     cam.begin();
     
+   
+    ofEnableDepthTest();
+    
+    ofSetCylinderResolution(ofMap(ofGetMouseX(), 0,ofGetWidth(), 1, 100),ofMap(ofGetMouseY(), 0,ofGetHeight(), 1, 100));
+
+    ofFill();
+    ofSetColor(200,0,120);
     ofDrawCylinder(200, 200, 0, 100, 300);
+    
+    ofSetColor(0);
+    ofNoFill();
+    ofSetLineWidth(8);
+    ofDrawCylinder(200, 200, 0, 100, 300);
+    ofDrawRectRounded(5675, 789, 789, 789, 789);
     cam.end();
     
-}
 
-//--------------------------------------------------------------
+}
+//--------------------------------------------------
 void ofApp::keyPressed(int key){
     seed ++;
 }
+//--------------------------------------------------
+void ofApp::keyReleased(int key){}
+//--------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){}
+//--------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){}
+//--------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){}
+//--------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){}
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
+//--------------------------------------------------
+void ofApp::mouseEntered(int x, int y){}
 
 //--------------------------------------------------------------
 void ofApp::mouseExited(int x, int y){
